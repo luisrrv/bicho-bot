45d89035b2110462aba02d7d222ca0d6b1cffe8b
Luis Rodriguez <rodluisr@gmail.com>
2023-03-23 23:47:24 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,36 @@
-import twitter
+# import twitter
+import tweepy
+import requests
 import random
-api = twitter.Api(
-    consumer_key='Zefk6DzixmX6NPv7MDIU0cpDK',
-    consumer_secret='1SsyR1T3yRwh25hVgYWjGKQb7bLvEUWS9n5RIOGCG7uZG5Gwzo',
-    access_token_key='1584892427801747456-43M5XUCqa62Fm1JhWOJ3dRhADXAHvQ',
-    access_token_secret='a8tVY7gigSmowiPaTNT0OjDu7aRhoCFWEht1FqG5hShdC'          
-)
-tweets = [
-    "SIUUUUUU!"
-    "Quizás me odian porque soy muy bueno.",
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+
+# Authenticate with the Twitter API
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+text_tweets = [
     # "La experiencia te hace entender que jugando en equipo y siendo solidario se alcanzan mayores objetivos.",
-    "Tu amor me hace fuerte, tu odio me hace imparable.",
-    "Quizás me odian porque soy muy bueno.",
     # "No soy un perfeccionista, pero me gusta sentir que las cosas se hacen bien. Más que eso, siento una necesidad interminable de aprender, de mejorar, de involucrarme, no solo para agradar al entrenador y a los aficionados, sino para sentirme satisfecho conmigo mismo.",
-    "Yo sé que al que le gusta el fútbol, le gusto yo.",
     # "El talento no lo es todo. Lo puedes tener desde la cuna, pero es necesario aprender el oficio para ser el mejor.",
     # "Prácticamente no tengo vida privada. Ya estoy acostumbrado a ello. Si, a veces es duro, pero es la elección que tomé.",
-    "¿Por qué mentir? No voy a ser un hipócrita y decir lo opuesto a lo que pienso, como hacen otros.",
     # "Soy consciente de que sean cuales sean las circunstancias, siempre habrá especulaciones sobre mi.",
-    "Demasiada humildad es un defecto.",
     # "Se que soy un buen profesional, se que nadie es tan duro conmigo como yo mismo y eso nunca va a cambiar.",
+    "SIUUUUUU!"
+    "Quizás me odian porque soy muy bueno.",
+    "Tu amor me hace fuerte, tu odio me hace imparable.",
+    "Quizás me odian porque soy muy bueno.",
+    "Yo sé que al que le gusta el fútbol, le gusto yo.",
+    "¿Por qué mentir? No voy a ser un hipócrita y decir lo opuesto a lo que pienso, como hacen otros.",
+    "Demasiada humildad es un defecto.",
     "Nunca he escondido el hecho de que es mi intención ser el mejor.",
     "Què hace una belleza como tú en un lugar tan sucio como mi mente?",
     "No soy Cristobal Colón, pero sí hay 1492 razones para quererte.",
@@ -30,7 +41,33 @@ tweets = [
     "Quieres ser el siii de mi uuuuu?",
     "Si Dios no exiscte por qué el bicho se llama Cristiano Ronaldo y no Ateo Ronaldo?",
 ]
-# Choose a random tweet
-tweet = random.choice(tweets)
-# Post the tweet
-api.PostUpdate(tweet)
+pic_tweets = [
+    # ['Hoy juega el bicho con Portugal SIIUUUUU!','https://japannews.yomiuri.co.jp/wp-content/uploads/2022/11/2022-11-24T172911Z_309460964_UP1EIBO1CKL9S_RTRMADP_3_SOCCER-WORLDCUP-POR-GHA-REPORT.jpg'],
+    ['Buenos dias bichologos','https://media.tenor.com/1nwjs8in1DUAAAAd/el-bicho-tomando-jugo-cr7.gif'],
+    ['','https://images3.memedroid.com/images/UPLOADED771/63ce6e66ed7b1.jpeg'],
+    ['Imagenes que puedes escuchar','https://static1.personality-database.com/profile_images/33adafc0e1534a9db3e9e2250ca0b054.png'],
+]
+# Choose a random tweet and Post it
+choice = random.choice(['text','pic'])
+if choice == 'text':
+    tweet = random.choice(text_tweets)
+    api.update_status(tweet)
+elif choice =='pic':
+    tweet = random.choice(pic_tweets)
+    # download image
+    filename = 'temp.jpg'
+    request = requests.get(tweet[1], stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
+
+        media = api.media_upload(filename)
+        api.update_status(status=tweet[0], media_ids=[media.media_id])
+        os.remove(filename)
+    else:
+        print("Unable to download image")
+else:
+    tweet = random.choice(text_tweets)
+    api.update_status(tweet)
+
